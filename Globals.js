894056167024deaa473262acd6edf8bf4a1473a1
Ronald Heiney <ronaldheiney@Ronalds-MacBook-Pro.local>
2019-08-19 15:35:03 -0600

    First commit

---
+++ Globals.js
@@ -0,0 +1,5 @@
+// https://stackoverflow.com/questions/33492943/global-variable-constant-in-react-native
+export default {
+  WATSON_DISCOVERY_CREDENTIALS:
+    'apikey:oK8nYKY6JoraeZ63H5uJM68t8yDV_cFSnrR1XnCUXhs7',
+};
